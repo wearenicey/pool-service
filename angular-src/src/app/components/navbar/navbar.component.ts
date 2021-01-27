1e8df889a3a825aae66024cb92319bc3ad92b7dd
Goran <gbabarogic@hmail.com>
2021-01-27 22:26:11 +0100

    novi typo sistem + heading izmene

--- angular-src/src/app/components/navbar/navbar.component.ts
+++ angular-src/src/app/components/navbar/navbar.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {faPhone, faEnvelope} from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-navbar',
@@ -6,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navbar.component.scss']
 })
 export class NavbarComponent implements OnInit {
+
+  faPhone = faPhone;
+  faEnvelope = faEnvelope;
 
   constructor() { }
 
